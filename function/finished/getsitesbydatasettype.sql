273ccffb1d5aa51e9a564a77fa35ed8da2cd6455
Simon <simon.j.goring@gmail.com>
2018-06-27 22:12:07 -0700

    Mostly for the structure.  Affed the finished folder.

---
+++ function/finished/getsitesbydatasettype.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION ti.getsitesbydatasettype(datasettypeid int)
+RETURNS TABLE (
+  siteid int,
+  sitename character varying,
+  latitude float,
+  longitude float,
+  altitude float,
+  area float
+) AS $$
+SELECT
+  sts.siteid AS siteid,
+  sts.sitename AS sitename,
+  ST_Y(ST_CENTROID(sts.geom)) AS latitude,
+  ST_X(ST_CENTROID(sts.geom)) AS longitude,
+  sts.altitude,
+  sts.area
+FROM ndb.datasets AS ds
+  INNER JOIN ndb.dslinks AS scd ON ds.collectionunitid = scd.collectionunitid
+  INNER JOIN ndb.sites AS sts ON scd.siteid = sts.siteid
+WHERE datasettypeid = ds.datasettypeid
+GROUP BY sts.siteid
+$$ LANGUAGE SQL;
