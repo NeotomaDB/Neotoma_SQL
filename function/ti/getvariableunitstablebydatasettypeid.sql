152726d8c23efc3167949e95219a21cc4db5d9e0
Simon <simon.j.goring@gmail.com>
2018-07-19 22:55:14 -0700

    Added a printing option to the python script and 7 queries.

---
+++ function/ti/getvariableunitstablebydatasettypeid.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE FUNCTION ti.getvariableunitstablebydatasettypeid(datasettypeid integer)
+ RETURNS TABLE(variableunits character varying)
+ LANGUAGE sql
+AS $function$
+SELECT vu.variableunits
+FROM
+  ndb.unitsdatasettypes AS udt
+  INNER JOIN ndb.variableunits AS vu ON udt.variableunitsid = vu.variableunitsid
+WHERE udt.datasettypeid = datasettypeid
+
+$function$
