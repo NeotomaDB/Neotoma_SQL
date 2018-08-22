9405a636ab80cd9837ca5bc33c1bdfc0432f1e83
Jack Williams <jww@geography.wisc.edu>
2018-08-22 17:05:57 -0500

    migrated gettaph...bydatasettype

---
+++ function/ti/gettaphonomicsystembydatasettype.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION ti.gettaphonomicsystembydatasettype(_datasettypeid integer)
+ RETURNS TABLE(taphonomicsystemid integer, taphonomicsystem character varying,
+			  notes character varying)
+ LANGUAGE sql
+ AS $function$
+
+ SELECT
+ 	tsy.taphonomicsystemid,
+	tsy.taphonomicsystem,
+	tsy.notes
+ FROM         ndb.taphonomicsystemsdatasettypes AS tsd INNER JOIN
+              ndb.taphonomicsystems AS tsy ON tsd.taphonomicsystemid = tsy.taphonomicsystemid
+ WHERE     tsd.datasettypeid = _datasettypeid
+
+$function$
