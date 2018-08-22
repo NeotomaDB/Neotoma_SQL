1a0ca382812415fc74511deb79b12cd429a85b92
Jack Williams <jww@geography.wisc.edu>
2018-08-22 15:19:00 -0500

    migrated getrocktypebyid

---
+++ function/ti/getrocktypebyid.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION ti.getrocktypebyid(_rocktypeid integer)
+ RETURNS TABLE(rocktypeid integer, rocktype character varying, higherrocktypeid integer, description character varying)
+
+ LANGUAGE sql
+AS $function$
+
+SELECT
+	rt.rocktypeid,
+	rt.rocktype,
+	rt.higherrocktypeid,
+	rt.description
+FROM      ndb.rocktypes as rt
+WHERE     rt.rocktypeid = _rocktypeid
+
+$function$
