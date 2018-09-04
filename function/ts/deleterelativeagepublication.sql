11de09ee50dd748d5f566910a99987d832094035
annaegeorge <aeg0495@gmail.com>
2018-09-04 13:32:32 -0500

    adding Sept 4 ts SQL

---
+++ function/ts/deleterelativeagepublication.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE FUNCTION ts.deleterelativeagepublication (_relativeageid integer, _publicationid integer)
+ RETURNS void
+ LANGUAGE sql
+AS $function$
+DELETE FROM ndb.relativeagepublications AS rap
+WHERE rap.relativeageid = _relativeageid AND rap.publicationid = _publicationid
+$function$
