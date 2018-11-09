cb18d69cc8fb4d27d81b5a7eff1dd511bfe844fb
annaegeorge <aeg0495@gmail.com>
2018-11-09 10:50:05 -0600

    more sqls

---
+++ function/ts/inserteventchronology.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION ts.inserteventchronology(_analysisunitid integer, 
+													_eventid integer,
+													_chroncontrolid integer,
+													_notes character varying = null)
+
+ RETURNS integer
+ LANGUAGE sql
+AS $function$
+  INSERT INTO ndb.eventchronology (analysisunitid, eventid, chroncontrolid, notes)
+  VALUES (_analysisunitid, _eventid, _chroncontrolid, _notes)
+  RETURNING eventchronologyid
+$function$;
