6bcc340e5c9a8cfd3eda57d54bde35f565e165d0
Simon <simon.j.goring@gmail.com>
2018-12-14 13:37:44 -0800

    Added pulled functions from the DB and fixed old ones.

--- function/ts/updaterelativeage.sql
+++ function/ts/updaterelativeage.sql
@@ -3,15 +3,21 @@ CREATE OR REPLACE FUNCTION ts.updaterelativeage(
   _relativeageunitid integer,
   _relativeagescaleid integer,
   _relativeage character varying,
-  _14cageyounger float = null,
-  _14ageolder float = null,
+  _c14ageyounger float = null,
+  _c14ageolder float = null,
   _calageyounger float = null,
   _calageolder float = null,
-  _notes = null)
+  _notes character varying = null)
  RETURNS void
  LANGUAGE sql
 AS $function$
 	UPDATE ndb.relativeages AS ra
-	SET   relativeageunitid = _relativeageunitid, relativeagescaleid = _relativeagescaleid, relativeage = _relativeage, c14ageyounger = _c14ageyounger, c14ageolder = _c14ageolder, calageyounger = _calageyounger, calageolder = _calageolder, notes = _notes
+	SET   relativeageunitid = _relativeageunitid,
+        relativeagescaleid = _relativeagescaleid,
+        relativeage = _relativeage,
+        c14ageyounger = _c14ageyounger,
+        c14ageolder = _c14ageolder,
+        calageyounger = _calageyounger,
+        calageolder = _calageolder, notes = _notes
 	WHERE ra.relativeageid = _relativeageid
 $function$;
