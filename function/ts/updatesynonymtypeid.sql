35dc938c019be7f6b51647f36ea0976304cdd85f
AdrianKGeorge <aeg0495@gmail.com>
2018-12-14 12:18:39 -0600

    pushing a few ts sqls

---
+++ function/ts/updatesynonymtypeid.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE FUNCTION ts.updatesynonymtypeid(_synonymid integer, _synonymtypeid integer)
+	RETURNS void
+	LANGUAGE sql
+AS $function$
+	UPDATE ndb.synonyms AS syn
+	SET    synonymtypeid = _synonymtypeid
+	WHERE  syn.synonymid = _synonymid
+$function$;
