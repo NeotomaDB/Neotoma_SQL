ff4808e8651155e5603831139e8e84ea016fa4b1
Simon <simon.j.goring@gmail.com>
2018-12-14 21:01:22 -0800

    Added five more functions/

---
+++ function/ts/insertsteward.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION ts.insertsteward(
+  _contactid int,
+  _username CHARACTER VARYING,
+  _password CHARACTER VARYING,
+  _taxonomyexpert smallint,
+  _databaseid int)
+RETURNS void
+LANGUAGE sql
+AS $function$
+
+  insert into ti.stewards(contactid, username, pwd, taxonomyexpert)
+  values      (_contactid, _username, _password, _taxonomyexpert);
+
+  insert into ti.stewarddatabases(stewardid, databaseid)
+  values      ((select stewardid from ti.stewards where (contactid = _contactid)), _databaseid)
+$function$
