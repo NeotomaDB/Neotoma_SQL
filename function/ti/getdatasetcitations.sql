32be87974afaa946da627cc522122108e44a5b54
Simon <simon.j.goring@gmail.com>
2019-04-03 11:32:34 -0700

    Re-formatted getdatasetcitations and fixed smallint/boolean issue.

--- function/ti/getdatasetcitations.sql
+++ function/ti/getdatasetcitations.sql
@@ -1,9 +1,13 @@
 CREATE OR REPLACE FUNCTION ti.getdatasetcitations(_datasetid integer)
- RETURNS TABLE(primarypub smallint, publicationid integer, citation text)
+ RETURNS TABLE(primarypub    boolean,
+               publicationid integer,
+               citation      text)
  LANGUAGE sql
 AS $function$
-SELECT ndb.datasetpublications.primarypub, ndb.publications.publicationid, ndb.publications.citation
-FROM ndb.datasetpublications INNER JOIN
-     ndb.publications ON ndb.datasetpublications.publicationid = ndb.publications.publicationid
-WHERE ndb.datasetpublications.datasetid = _datasetid;
+SELECT dpu.primarypub,
+       pub.publicationid,
+       pub.citation
+FROM  ndb.datasetpublications AS dpu
+  INNER JOIN ndb.publications AS pub ON dpu.publicationid = pub.publicationid
+WHERE dpu.datasetid = _datasetid;
 $function$
