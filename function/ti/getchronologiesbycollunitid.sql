7938d802a55b1e99b4eb0dc9750639a25ce1851d
Simon <simon.j.goring@gmail.com>
2019-04-03 11:22:37 -0700

    `smallint`/`boolean` error fixed.

--- function/ti/getchronologiesbycollunitid.sql
+++ function/ti/getchronologiesbycollunitid.sql
@@ -1,9 +1,18 @@
 CREATE OR REPLACE FUNCTION ti.getchronologiesbycollunitid(collunitid integer)
- RETURNS TABLE(chronologyid integer, agetype character varying, chronologyname character varying, isdefault smallint, agemodel character varying, ageboundolder integer, ageboundyounger integer, contactid integer, dateprepared character varying, notes text)
+ RETURNS TABLE(chronologyid integer,
+               agetype character varying,
+               chronologyname character varying,
+               isdefault boolean,
+               agemodel character varying,
+               ageboundolder integer,
+               ageboundyounger integer,
+               contactid integer,
+               dateprepared character varying,
+               notes text)
  LANGUAGE sql
 AS $function$
-SELECT ndb.chronologies.chronologyid, ndb.agetypes.agetype, ndb.chronologies.chronologyname, 
-       ndb.chronologies.isdefault, ndb.chronologies.agemodel, ndb.chronologies.ageboundolder, ndb.chronologies.ageboundyounger,   
+SELECT ndb.chronologies.chronologyid, ndb.agetypes.agetype, ndb.chronologies.chronologyname,
+       ndb.chronologies.isdefault, ndb.chronologies.agemodel, ndb.chronologies.ageboundolder, ndb.chronologies.ageboundyounger,
        ndb.chronologies.contactid, ndb.chronologies.dateprepared::varchar(10) AS dateprepared, ndb.chronologies.notes
 FROM ndb.chronologies INNER JOIN ndb.agetypes ON ndb.chronologies.agetypeid = ndb.agetypes.agetypeid
 WHERE ndb.chronologies.collectionunitid = collunitid
