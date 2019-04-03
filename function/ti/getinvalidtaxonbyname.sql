d7f4fd3110007289dca7947ac1fe43e235d35c97
Simon <simon.j.goring@gmail.com>
2019-04-03 12:31:04 -0700

    Added a bash script to replace all instances of `smallint` and added the script to a `helpers` folder.

--- function/ti/getinvalidtaxonbyname.sql
+++ function/ti/getinvalidtaxonbyname.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE FUNCTION ti.getinvalidtaxonbyname(_taxonname character varying)
- RETURNS TABLE(taxonid integer, taxoncode character varying, taxonname character varying, author character varying, valid smallint, highertaxonid integer, extinct smallint, taxagroupid character varying, publicationid integer, validatorid integer, validatedate character varying, notes text)
+ RETURNS TABLE(taxonid integer, taxoncode character varying, taxonname character varying, author character varying, valid boolean, highertaxonid integer, extinct boolean, taxagroupid character varying, publicationid integer, validatorid integer, validatedate character varying, notes text)
  LANGUAGE sql
 AS $function$
 
