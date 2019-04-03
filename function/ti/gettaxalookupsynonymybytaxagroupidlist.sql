1154353ebb361c8e99248761dd6adf586af0c44a
Simon <simon.j.goring@gmail.com>
2019-04-03 12:02:35 -0700

    More smallint fixes, but with 1s and 0s.

--- function/ti/gettaxalookupsynonymybytaxagroupidlist.sql
+++ function/ti/gettaxalookupsynonymybytaxagroupidlist.sql
@@ -1,16 +1,18 @@
 CREATE OR REPLACE FUNCTION ti.gettaxalookupsynonymybytaxagroupidlist(taxagrouplist character varying)
- RETURNS TABLE(taxonid integer, taxonname character varying, validtaxonid integer)
+ RETURNS TABLE(taxonid integer,
+               taxonname character varying,
+               validtaxonid integer)
  LANGUAGE sql
 AS $function$
 
-SELECT tx.taxonid, 
-        tx.taxonname, 
-		sy.validtaxonid
-from 
+SELECT tx.taxonid,
+       tx.taxonname,
+		   sy.validtaxonid
+from
   ndb.taxa AS tx
   inner join ndb.synonyms AS sy on tx.taxonid = sy.invalidtaxonid
-where      
-  tx.valid = 0 AND 
+where
+  tx.valid = False AND
   (tx.taxagroupid in (SELECT unnest(string_to_array(taxagrouplist,'$'))))
 
 $function$
