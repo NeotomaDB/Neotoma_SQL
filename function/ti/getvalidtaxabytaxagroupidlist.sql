d1173804835e8c568c33ea5f4b3136d65acee811
Simon <simon.j.goring@gmail.com>
2019-04-03 11:58:41 -0700

    smallint/boolean and formatting for validtaxabytaonidlist.

--- function/ti/getvalidtaxabytaxagroupidlist.sql
+++ function/ti/getvalidtaxabytaxagroupidlist.sql
@@ -1,24 +1,35 @@
 CREATE OR REPLACE FUNCTION ti.getvalidtaxabytaxagroupidlist(taxagrouplist character varying)
- RETURNS TABLE(taxonid integer, taxoncode character varying, taxonname character varying, author character varying, valid smallint, highertaxonid integer, extinct smallint, taxagroupid character varying, publicationid integer, validatorid integer, validatedate character varying, notes character varying)
+ RETURNS TABLE(taxonid integer,
+               taxoncode character varying,
+               taxonname character varying,
+               author character varying,
+               valid boolean,
+               highertaxonid integer,
+               extinct boolean,
+               taxagroupid character varying,
+               publicationid integer,
+               validatorid integer,
+               validatedate character varying,
+               notes character varying)
  LANGUAGE sql
 AS $function$
 select
-  tx.taxonid, 
-  tx.taxoncode, 
-  tx.taxonname, 
-  tx.author, 
-  tx.valid, 
-  tx.highertaxonid, 
+  tx.taxonid,
+  tx.taxoncode,
+  tx.taxonname,
+  tx.author,
+  tx.valid,
+  tx.highertaxonid,
   tx.extinct, 
-  tx.taxagroupid, 
+  tx.taxagroupid,
   tx.publicationid,
-  tx.validatorid, 
-  TO_CHAR(tx.validatedate, 'YYYY-MM-DD HH:MI:SS') as validatedate, 
+  tx.validatorid,
+  TO_CHAR(tx.validatedate, 'YYYY-MM-DD HH:MI:SS') as validatedate,
   notes
 FROM
   ndb.taxa AS tx
 WHERE
-  (tx.valid = 1) AND 
+  (tx.valid = 1) AND
   (tx.taxagroupid IN (
     SELECT unnest(string_to_array(taxagrouplist,'$')))
   )
