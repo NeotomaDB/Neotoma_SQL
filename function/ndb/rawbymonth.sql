39581284ecbe70e18fbeeda2377e8243b6c10e40
Simon <simon.j.goring@gmail.com>
2019-05-15 12:52:01 -0700

    Modified rawbymonth function to add a CTE. Was accidentally overcounting things, needed to unnest the columns.

--- function/ndb/rawbymonth.sql
+++ function/ndb/rawbymonth.sql
@@ -1,24 +1,37 @@
 CREATE OR REPLACE FUNCTION ndb.rawbymonth(startperiod integer default 0,
                                                 endperiod integer default 1)
-RETURNS TABLE (summary json)
+RETURNS TABLE (datasets bigint,
+               sites bigint,
+             publications, bigint,
+           authors bigint,
+         observations bigint)
 AS
 $function$
-SELECT ds.datasetid,
+WITH rsum AS (
+  SELECT ds.datasetid,
         dsl.siteid,
-        COUNT(DISTINCT(dsp.publicationid)) AS publications,
-        COUNT(DISTINCT(pua.contactid)) AS authors,
+        array_agg(DISTINCT dsp.publicationid) AS publications,
+        array_agg(DISTINCT pua.contactid) AS authors,
         gpd.path[1] AS countrygpid,
         smp.observations
-FROM ndb.datasets AS ds
-JOIN ndb.datasetsubmissions AS dss on dss.datasetid = ds.datasetid
-JOIN ndb.dslinks AS dsl ON dsl.datasetid = ds.datasetid
-JOIN ndb.dsdampdata AS smp ON ds.datasetid = smp.datasetid
-LEFT OUTER JOIN ndb.datasetpublications AS dsp ON dsp.datasetid = ds.datasetid
-LEFT OUTER JOIN ndb.publicationauthors AS pua ON dsp.publicationid = pua.publicationid
-JOIN ndb.sitegeopolitical AS sgp ON dsl.siteid = sgp.siteid
-JOIN ndb.geopoldepth AS gpd ON gpd.geopoliticalid = sgp.geopoliticalid
-WHERE
-  EXTRACT(year from AGE(NOW(), dss.submissiondate))*12 +
-   EXTRACT(month from AGE(NOW(), dss.submissiondate)) BETWEEN startperiod and endperiod
-GROUP BY ds.datasetid, dsl.siteid, dsl.siteid, gpd.path[1], smp.observations
+  FROM ndb.datasets AS ds
+  JOIN ndb.datasetsubmissions AS dss on dss.datasetid = ds.datasetid
+  JOIN ndb.dslinks AS dsl ON dsl.datasetid = ds.datasetid
+  JOIN ndb.dssampdata AS smp ON ds.datasetid = smp.datasetid
+  LEFT OUTER JOIN ndb.datasetpublications AS dsp ON dsp.datasetid = ds.datasetid
+  LEFT OUTER JOIN ndb.publicationauthors AS pua ON dsp.publicationid = pua.publicationid
+  JOIN ndb.sitegeopolitical AS sgp ON dsl.siteid = sgp.siteid
+  JOIN ndb.geopoldepth AS gpd ON gpd.geopoliticalid = sgp.geopoliticalid
+  WHERE
+    ds.datasetid < 10
+  GROUP BY ds.datasetid, dsl.siteid, dsl.siteid, gpd.path[1], smp.observations)
+SELECT COUNT(DISTINCT datasetid) AS datasets,
+       COUNT(DISTINCT siteid) AS sites,
+       COUNT(DISTINCT unpublications) AS publications,
+       COUNT(DISTINCT unauthors) AS authors,
+       COUNT(DISTINCT countrygpid) AS countries,
+       SUM(observations) AS observations
+FROM rsum,
+						unnest(authors) AS unauthors,
+						unnest(publications) AS unpublications
 $function$ LANGUAGE SQL;
