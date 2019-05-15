b86c2f792ccac410ded119292e5e7c5df4a603b3
Simon <simon.j.goring@gmail.com>
2019-05-15 11:47:34 -0700

    Added a view to give a running tally of the number of samples and data observations (rows in `data`) associated with each datasetid.

---
+++ function/ndb/dssampdata.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE VIEW ndb.dssampdata AS
+SELECT ds.datasetid,
+       count(DISTINCT smp.sampleid) AS samples,
+	   count(DISTINCT dt.dataid) AS observations
+FROM ndb.datasets AS ds
+JOIN ndb.samples AS smp ON smp.datasetid = ds.datasetid
+JOIN ndb.data AS dt ON dt.sampleid = smp.sampleid
+GROUP BY ds.datasetid;
+GRANT SELECT ON ndb.dssampdata TO neotomawsreader;
