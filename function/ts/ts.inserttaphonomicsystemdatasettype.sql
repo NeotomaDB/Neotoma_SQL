309523fef85627774fa42ef97c244ab4d8f8a5ba
Simon <simon.j.goring@gmail.com>
2019-07-12 11:21:47 -0700

    Changed accidental renamng of `ts` functions. Also added some new functions to the repo that had been added directly to the db server.

--- function/ts/ts.inserttaphonomicsystemdatasettype.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE FUNCTION ts.inserttaphonomicsystemdatasettype(_datasettypeid int,
-	_taphonomicsystemid int) RETURNS void
-AS $$
-INSERT INTO ndb.taphonomicsystemsdatasettypes
-	(datasettypeid, taphonomicsystemid)
-VALUES (_datasettypeid, _taphonomicsystemid)
-$$ LANGUAGE SQL;
