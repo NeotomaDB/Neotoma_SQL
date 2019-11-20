1ffc5b50689a2974249bc634193c7c82f3c1d918
Simon <simon.j.goring@gmail.com>
2019-11-20 09:10:03 -0800

    More small fixes from the google spreadsheet. Fixing case sensitivity and aliases.

--- function/ti/getgeochronbydatasetid.sql
+++ function/ti/getgeochronbydatasetid.sql
@@ -1,17 +1,42 @@
 CREATE OR REPLACE FUNCTION ti.getgeochronbydatasetid(_datasetid integer)
- RETURNS TABLE(geochronid integer, geochrontypeid integer, geochrontype character varying, agetype character varying, depth double precision, thickness double precision, analysisunitid integer, analysisunitname character varying, age double precision, errorolder double precision, erroryounger double precision, infinite boolean, labnumber character varying, materialdated character varying, notes text)
+ RETURNS TABLE(geochronid integer,
+               geochrontypeid integer,
+               geochrontype character varying,
+               agetype character varying,
+               depth double precision,
+               thickness double precision,
+               analysisunitid integer,
+               analysisunitname character varying,
+               age double precision,
+               errorolder double precision,
+               erroryounger double precision,
+               infinite boolean,
+               labnumber character varying,
+               materialdated character varying,
+               notes text)
  LANGUAGE sql
 AS $function$
-
-select     ndb.geochronology.geochronid, ndb.geochronology.geochrontypeid, ndb.geochrontypes.geochrontype, ndb.agetypes.agetype, ndb.analysisunits.depth, 
-                      ndb.analysisunits.thickness, ndb.analysisunits.analysisunitid, ndb.analysisunits.analysisunitname, ndb.geochronology.age, ndb.geochronology.errorolder, ndb.geochronology.erroryounger, 
-                      ndb.geochronology.infinite, ndb.geochronology.labnumber, ndb.geochronology.materialdated, ndb.geochronology.notes
-from         ndb.geochronology inner join
-                      ndb.samples on ndb.geochronology.sampleid = ndb.samples.sampleid inner join
-                      ndb.datasets on ndb.samples.datasetid = ndb.datasets.datasetid inner join
-                      ndb.geochrontypes on ndb.geochronology.geochrontypeid = ndb.geochrontypes.geochrontypeid inner join
-                      ndb.agetypes on ndb.geochronology.agetypeid = ndb.agetypes.agetypeid inner join
-                      ndb.analysisunits on ndb.samples.analysisunitid = ndb.analysisunits.analysisunitid
-where     (ndb.datasets.datasetid = $1)
+  select gc.geochronid,
+         gct.geochrontypeid,
+         gct.geochrontype,
+         aty.agetype,
+         au.depth,
+         au.thickness,
+         au.analysisunitid,
+         au.analysisunitname,
+         gc.age,
+         gc.errorolder,
+         gc.erroryounger,
+         gc.infinite,
+         gc.labnumber,
+         gc.materialdated,
+         gc.notes
+  from         ndb.geochronology AS gc
+    inner join       ndb.samples AS smp on        gc.sampleid = smp.sampleid
+    inner join      ndb.datasets AS ds  on      smp.datasetid = ds.datasetid
+    inner join ndb.geochrontypes AS gct on  gc.geochrontypeid = gct.geochrontypeid
+    inner join      ndb.agetypes AS aty on       gc.agetypeid = aty.agetypeid
+    inner join ndb.analysisunits AS au  on smp.analysisunitid = au.analysisunitid
+  where (ds.datasetid = $1)
 
 $function$;
