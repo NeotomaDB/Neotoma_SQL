c9dd5773b29ee9836b4a37187a158cd17ff4c828
Simon <simon.j.goring@gmail.com>
2019-11-20 09:10:03 -0800

    More small fixes from the google spreadsheet. Fixing case sensitivity and aliases.

--- function/ti/getnextlowergeopolcountbynames.sql
+++ function/ti/getnextlowergeopolcountbynames.sql
@@ -2,9 +2,10 @@ CREATE OR REPLACE FUNCTION ti.getnextlowergeopolcountbynames(_name1 character va
  RETURNS TABLE (count bigint)
  LANGUAGE sql
 AS $function$
-SELECT COUNT(geopoliticalunits_1.geopoliticalname) AS count
-FROM ndb.geopoliticalunits INNER JOIN
-		ndb.geopoliticalunits AS geopoliticalunits_1 ON ndb.geopoliticalunits.geopoliticalid = geopoliticalunits_1.highergeopoliticalid
-WHERE (ndb.geopoliticalunits.geopoliticalname = _name1) AND (ndb.geopoliticalunits.rank = _rank1) AND
-		(geopoliticalunits_1.geopoliticalname = _name2) AND (geopoliticalunits_1.rank = _rank2);
+  SELECT COUNT(gpu_1.geopoliticalname) AS count
+  FROM ndb.geopoliticalunits AS gpu
+    INNER JOIN
+  		ndb.geopoliticalunits AS gpu_1 ON gpu.geopoliticalid = gpu_1.highergeopoliticalid
+  WHERE (gpu.geopoliticalname ILIKE _name1) AND (gpu.rank = _rank1) AND
+  		(gpu_1.geopoliticalname ILIKE _name2) AND (gpu_1.rank = _rank2);
 $function$;
