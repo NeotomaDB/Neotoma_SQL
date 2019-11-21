420e4a6c6b00f5e01b87a17d38c0e110ecbaab30
Simon <simon.j.goring@gmail.com>
2019-11-21 12:26:26 -0800

    Updated the getprocedureinputparams.

--- function/ti/getprocedureinputparams.sql
+++ function/ti/getprocedureinputparams.sql
@@ -1,22 +1,23 @@
 CREATE OR REPLACE FUNCTION ti.getprocedureinputparams(_procedurename character varying)
- RETURNS TABLE(params text)
- LANGUAGE plpgsql
+ RETURNS TABLE(name character varying,
+               type character varying,
+               isdefault boolean,
+               paramorder bigint)
+ LANGUAGE sql
 AS $function$
-
-DECLARE
-
-	sch text := split_part(_procedurename, '.', 1);
-	func text := split_part(_procedurename, '.', 2);
-
-BEGIN
-
-	RETURN QUERY
-	SELECT unnest(regexp_split_to_array(pg_get_function_arguments(p.oid), ', ')) AS params
-	FROM pg_catalog.pg_namespace n JOIN  pg_catalog.pg_proc p
-	ON p.pronamespace = n.oid
-	WHERE p.proname ILIKE func AND n.nspname ILIKE sch;
-
-
-END;
-
+WITH params AS (
+	SELECT  _procedurename AS procname,
+			regexp_split_to_array(oidvectortypes(p.proargtypes), ', ') AS argtypes,
+			p.proargnames AS argnames,
+			regexp_split_to_array(pg_get_expr(p.proargdefaults,0), ',') AS isdefault
+		FROM pg_catalog.pg_namespace AS n
+		  JOIN  pg_catalog.pg_proc p
+		ON p.pronamespace = n.oid
+		WHERE p.proname ILIKE split_part(_procedurename, '.', 2) AND n.nspname ILIKE split_part(_procedurename, '.', 1))
+    SELECT u.name,
+           u.type,
+           u.def IS NOT NULL AS default,
+           ROW_NUMBER() OVER (ORDER BY name) AS order
+    FROM  unnest((SELECT argtypes FROM params), (SELECT argnames FROM params),(SELECT isdefault FROM params)) AS u(type, name, def)
+    WHERE type IS NOT NULL
 $function$
