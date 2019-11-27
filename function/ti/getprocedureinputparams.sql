0711ef870d024c39705eddfcb6b08caf5ba81c31
Michael Stryker <michael.s.stryker@gmail.com>
2019-11-27 12:23:05 -0500

    fixed parameter ordering  and isdefault return values

--- function/ti/getprocedureinputparams.sql
+++ function/ti/getprocedureinputparams.sql
@@ -5,19 +5,15 @@ CREATE OR REPLACE FUNCTION ti.getprocedureinputparams(_procedurename character v
                paramorder bigint)
  LANGUAGE sql
 AS $function$
-WITH params AS (
-	SELECT  _procedurename AS procname,
-			regexp_split_to_array(oidvectortypes(p.proargtypes), ', ') AS argtypes,
-			p.proargnames AS argnames,
-			regexp_split_to_array(pg_get_expr(p.proargdefaults,0), ',') AS isdefault
-		FROM pg_catalog.pg_namespace AS n
-		  JOIN  pg_catalog.pg_proc p
-		ON p.pronamespace = n.oid
-		WHERE p.proname ILIKE split_part(_procedurename, '.', 2) AND n.nspname ILIKE split_part(_procedurename, '.', 1))
-    SELECT u.name,
-           u.type,
-           u.def IS NOT NULL AS default,
-           ROW_NUMBER() OVER (ORDER BY name) AS order
-    FROM  unnest((SELECT argtypes FROM params), (SELECT argnames FROM params),(SELECT isdefault FROM params)) AS u(type, name, def)
-    WHERE type IS NOT NULL
+SELECT     
+    parameters.parameter_name,
+    parameters.data_type, 
+    (SELECT CASE WHEN parameters.parameter_default IS NOT NULL THEN TRUE ELSE FALSE END)  isdefault,
+    parameters.ordinal_position::bigint
+    
+FROM information_schema.routines
+    LEFT JOIN information_schema.parameters ON routines.specific_name=parameters.specific_name
+WHERE routines.specific_schema ILIKE split_part(_procedurename, '.', 1)
+    and routine_name ILIKE split_part(_procedurename, '.', 2)
+ORDER BY parameters.ordinal_position;
 $function$
