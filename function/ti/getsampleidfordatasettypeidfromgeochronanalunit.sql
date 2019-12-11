bbf5a6fe7e7675c822b11132005902f755143bfe
Stephen Crawford <src176@psu.edu>
2019-12-11 11:15:03 -0500

    2 more ti

---
+++ function/ti/getsampleidfordatasettypeidfromgeochronanalunit.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION ti.getsampleidfordatasettypeidfromgeochronanalunit(_geochronid int, _datasettypeid int)
+RETURNS TABLE(sampleid integer)
+LANGUAGE sql
+AS $function$
+
+SELECT samples_1.sampleid
+FROM ndb.geochronology INNER JOIN
+	ndb.samples ON ndb.geochronology.sampleid = ndb.samples.sampleid INNER JOIN
+	ndb.samples as samples_1 ON ndb.samples.analysisunitid = samples_1.analysisunitid INNER JOIN
+	ndb.datasets ON samples_1.datasetid = ndb.datasets.datasetid
+WHERE (ndb.geochronology.geochronid = _geochronid) AND (ndb.datasets.datasettypeid = _datasettypeid)
+
+$function$
