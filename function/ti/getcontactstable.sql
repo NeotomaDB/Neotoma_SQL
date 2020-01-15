374dd95726dcbca81991be23340e9a8a5d3ab288
Simon <simon.j.goring@gmail.com>
2020-01-15 07:41:27 -0800

    Adding ti functions pulled from neotomadev.

---
+++ function/ti/getcontactstable.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE FUNCTION ti.getcontactstable()
+ RETURNS TABLE(contactid integer, aliasid integer, contactname character varying, contactstatusid integer, familyname character varying, leadinginitials character varying, givennames character varying, suffix character varying, title character varying, phone character varying, fax character varying, email character varying, url character varying, address text, notes text, recdatecreated timestamp without time zone, recdatemodified timestamp without time zone)
+ LANGUAGE sql
+AS $function$
+SELECT *
+FROM ndb.contacts;
+$function$
