ab3a5dc7a42f3dc3e35ab0e7962cdd2f1e7afc9e
Simon <simon.j.goring@gmail.com>
2020-01-15 08:30:02 -0800

    Fixed typo in gettablemaxid

--- function/ti/gettablemaxid.sql
+++ function/ti/gettablemaxid.sql
@@ -1,14 +1,12 @@
 CREATE OR REPLACE FUNCTION ti.gettablemaxid(_tablename character varying, _columnname character varying)
  RETURNS TABLE(maxid INTEGER)
  LANGUAGE plpgsql
-AS $function$S
+AS $function$
 DECLARE maxid int;
 BEGIN
   RETURN QUERY
   EXECUTE format(
     'SELECT max(%s) as maxid FROM %s', _columnname, _tablename)
 	INTO maxid;
- 
-	
 END;
 $function$
