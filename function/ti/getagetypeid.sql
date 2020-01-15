e6b6a67da312045f22bf31df900278f8f0ab0c9a
Simon <simon.j.goring@gmail.com>
2020-01-15 09:15:00 -0800

    Fixes to connect_remote. This represents a first step at cleaning up the connect_remote issues. There are a set of functions that get re-written each time as we run the script.  This results from a difference in how Postgres returns the fucntion, vs how we send it out.  Most of this can be fixed, but effectively, the program runs as expected.

--- function/ti/getagetypeid.sql
+++ function/ti/getagetypeid.sql
@@ -1,9 +1,8 @@
 CREATE OR REPLACE FUNCTION ti.getagetypeid(_agetype character varying)
- RETURNS TABLE(agetypeid integer)
-AS $$
-
-SELECT agetypeid
-FROM  ndb.agetypes at
-WHERE at.agetype ILIKE $1;
-
-$$ LANGUAGE SQL;
+RETURNS TABLE(agetypeid integer)
+LANGUAGE sql
+AS $function$
+  SELECT agetypeid
+  FROM  ndb.agetypes at
+  WHERE at.agetype ILIKE $1;
+$function$
