412264dbd3c24fbaede4b821681d1020804640c9
Simon <simon.j.goring@gmail.com>
2020-02-13 11:12:40 -0800

    Updated dataset publications. Made variables lowercase and removed the if statement.

--- function/ts/insertdatasetpublication.sql
+++ function/ts/insertdatasetpublication.sql
@@ -1,28 +1,17 @@
+CREATE OR REPLACE FUNCTION ts.insertdatasetpublication(
+  _datasetid int,
+  _publicationid int,
+  _primarypub boolean)
 
-CREATE OR REPLACE FUNCTION ts.insertdatasetpublication(_DATASETID int, _PUBLICATIONID int, _PRIMARYPUB boolean)
 RETURNS VOID
-LANGUAGE 'plpgsql'
-
-    COST 100
-    VOLATILE     
+LANGUAGE sql
 AS $function$
-
-  DECLARE _nrecs integer;
-
-  BEGIN
-
-    SELECT COUNT(*) 
-    FROM ndb.datasetpublications 
-    GROUP BY datasetid, publicationid 
-    HAVING (datasetid = _datasetid) AND (publicationid = _publicationid)
-    INTO _nrecs;
-
-
-    IF _nrecs IS NULL THEN
-        INSERT INTO ndb.datasetpublications(datasetid, publicationid, primarypub)
-        VALUES      (_datasetid, _publicationid, _primarypub);
-    END IF;
-
-  END;
-
+  INSERT INTO ndb.datasetpublications(datasetid, publicationid, primarypub)
+  VALUES      (_datasetid, _publicationid, _primarypub)
+  WHERE
+    NOT EXISTS (
+      SELECT dsp.datasetid, dsp.publicationid
+      FROM ndb.datasetpublications AS dsp
+      WHERE (dsp.datasetid = _datasetid) AND (dsp.publicationid = _publicationid)
+    )
 $function$
