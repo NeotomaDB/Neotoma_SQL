7fdba9f492d6e7671d348c459f2d0f627cfa6527
Simon <simon.j.goring@gmail.com>
2020-02-14 16:05:57 -0800

    Made a mistake, submitted specimen data as dataset.

--- function/ti/getspecimenisotopedata.sql
+++ function/ti/getspecimenisotopedata.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION getspecimenisotopedataset(_datasetid integer)
+CREATE OR REPLACE FUNCTION getspecimenisotopedata(_datasetid integer)
 RETURNS TABLE (
   specimenid integer,
   analysisunit character varying,
