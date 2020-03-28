77a86d0643c2d5ec19940e3661136f11046bf18e
Michael Stryker <michael.s.stryker@gmail.com>
2020-03-28 16:24:54 -0400

    function param to boolean from small int

--- function/ts/insertsteward.sql
+++ function/ts/insertsteward.sql
@@ -1,11 +1,19 @@
-CREATE OR REPLACE FUNCTION ts.insertsteward(_contactid integer, _username character varying, _password character varying, _taxonomyexpert smallint, _databaseid integer)
- RETURNS void
- LANGUAGE sql
-AS $function$
+CREATE OR REPLACE FUNCTION ts.insertsteward(
+    _contactid integer,
+    _username character varying,
+    _password character varying,
+    _taxonomyexpert boolean,
+    _databaseid integer)
+    RETURNS void
+    LANGUAGE 'sql'
+
+    COST 100
+    VOLATILE 
+AS $BODY$
 
   insert into ti.stewards(contactid, username, pwd, taxonomyexpert)
   values      (_contactid, _username, _password, _taxonomyexpert);
 
   insert into ti.stewarddatabases(stewardid, databaseid)
   values      ((select stewardid from ti.stewards where (contactid = _contactid)), _databaseid)
-$function$
+$BODY$;
