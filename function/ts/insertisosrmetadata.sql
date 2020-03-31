b7efe3243c70b6b6e6f7b4213d340dd2c8dede54
Simon <simon.j.goring@gmail.com>
2020-03-31 15:00:15 -0700

    Updating functions based on spreadhseet.

--- function/ts/insertisosrmetadata.sql
+++ function/ts/insertisosrmetadata.sql
@@ -1,7 +1,15 @@
-CREATE OR REPLACE FUNCTION ts.insertisosrmetadata(_datasetid integer, _variableid integer, _srlocalvalue character varying DEFAULT NULL::character varying, _srlocalgeolcontext character varying DEFAULT NULL::character varying)
+CREATE OR REPLACE FUNCTION ts.insertisosrmetadata(_datasetid integer,
+    _variableid integer,
+    _srlocalvalue float DEFAULT NULL::float,
+    _srlocalvaluesd float DEFAULT NULL::float,
+    _srlocalgeolcontext character varying DEFAULT NULL::character varying)
  RETURNS void
  LANGUAGE sql
 AS $function$
-  INSERT INTO ndb.isosrmetadata(datasetid, variableid, srlocalvalue, srlocalgeolcontext)
-  VALUES (_datasetid, _variableid, _srlocalvalue, _srlocalgeolcontext)
+  INSERT INTO ndb.isosrmetadata(datasetid, variableid,
+                                srlocalvalue, srlocalvaluesd,
+                                srlocalgeolcontext)
+  VALUES (_datasetid, _variableid,
+          _srlocalvalue, _srlocalvaluesd,
+          _srlocalgeolcontext)
 $function$
--- function/ts/insertradiocarbon.sql
+++ function/ts/insertradiocarbon.sql
@@ -1,9 +1,18 @@
-CREATE OR REPLACE FUNCTION ts.insertradiocarbon(_geochronid integer, _radiocarbonmethodid integer DEFAULT NULL::integer, _percentc double precision DEFAULT NULL::double precision, _percentn double precision DEFAULT NULL::double precision, _delta13c double precision DEFAULT NULL::double precision, _delta15n double precision DEFAULT NULL::double precision, _percentcollagen double precision DEFAULT NULL::double precision, _reservoir double precision DEFAULT NULL::double precision)
+CREATE OR REPLACE FUNCTION ts.insertradiocarbon(_geochronid integer,
+                                                _radiocarbonmethodid integer DEFAULT NULL::integer,
+                                                _percentc double precision DEFAULT NULL::double precision,
+                                                _percentn double precision DEFAULT NULL::double precision,
+                                                _delta13c double precision DEFAULT NULL::double precision,
+                                                _delta15n double precision DEFAULT NULL::double precision,
+                                                _cnratio  double precision DEFAULT NULL::double precision,
+                                                _masscmg   double precision DEFAULT NULL::double precision,
+                                                _percentcollagen double precision DEFAULT NULL::double precision,
+                                                _reservoir double precision DEFAULT NULL::double precision)
  RETURNS void
  LANGUAGE sql
 AS $function$
   INSERT INTO ndb.radiocarbon(geochronid, radiocarbonmethodid, percentc,
-    percentn, delta13c, delta15n, percentcollagen, reservoir)
+    percentn, delta13c, delta15n, percentcollagen, reservoir, masscmg, cnratio)
   VALUES (_geochronid, _radiocarbonmethodid, _percentc, _percentn, _delta13c,
-    _delta15n, _percentcollagen, _reservoir)
+    _delta15n, _percentcollagen, _reservoir, _masscmg, _cnratio)
 $function$
