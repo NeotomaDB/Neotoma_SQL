98c82382764cd482c877c109952ade46085138d2
Simon <simon.j.goring@gmail.com>
2020-05-14 13:14:20 -0700

    Adding new files with datachecks.

--- function/ndb/datasetsummary.sql
+++ function/ndb/datasetsummary.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION ndb.datasetconstitdb(startperiod integer default 0,
+CREATE OR REPLACE FUNCTION ndb.datasetsummary(startperiod integer default 0,
                                                 endperiod integer default 1)
 RETURNS TABLE (databasename varchar,
                counts bigint)
@@ -6,8 +6,9 @@ AS
 $function$
   SELECT dst.datasettype, count(*)
   FROM ndb.datasets AS ds
+  JOIN ndb.datasettypes AS dst ON ds.datasettypeid = dst.datasettypeid
   JOIN ndb.datasetsubmissions AS dss on dss.datasetid = ds.datasetid
   JOIN ndb.constituentdatabases AS cdb ON cdb.databaseid = dss.databaseid
   WHERE EXTRACT(month from AGE(NOW(), dss.submissiondate)) BETWEEN 1 and 2
-  GROUP BY cdb.databasename
+  GROUP BY cdb.databasename, dst.datasettype
 $function$ LANGUAGE SQL;
