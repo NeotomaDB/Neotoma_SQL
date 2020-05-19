ff563491c056d0e8119d9768dd295658b3faf551
Simon Goring <goring@wisc.edu>
2020-05-18 21:28:13 -0700

    Updating functions and the script to fix error handling.

--- function/ts/updatepublicationauthor.sql
+++ function/ts/updatepublicationauthor.sql
@@ -1,4 +1,10 @@
-CREATE OR REPLACE FUNCTION ts.updatepublicationauthor(_authorid integer, _publicationid integer, _authororder integer, _familyname character varying, _initials character varying, _suffix character varying, _contactid integer)
+CREATE OR REPLACE FUNCTION ts.updatepublicationauthor(_authorid integer,
+                                                      _publicationid integer,
+                                                      _authororder integer,
+                                                      _familyname character varying,
+                                                      _contactid integer,
+                                                      _initials character varying DEFAULT NULL::character varying,
+                                                      _suffix character varying  DEFAULT NULL::character varying)
  RETURNS void
  LANGUAGE sql
 AS $function$
