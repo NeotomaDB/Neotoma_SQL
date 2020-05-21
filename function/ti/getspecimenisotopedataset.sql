68380935883d0012b8465cc0aecd6a06cb6cad63
Simon Goring <goring@wisc.edu>
2020-05-21 15:57:40 -0700

    Specimen isotopedataset should now be in the `ti` namespace.

--- function/ti/getspecimenisotopedataset.sql
+++ function/ti/getspecimenisotopedataset.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION getspecimenisotopedataset(_datasetid integer)
+CREATE OR REPLACE FUNCTION ti.getspecimenisotopedataset(_datasetid integer)
 RETURNS TABLE (
   specimenid integer,
   analysisunit character varying,
