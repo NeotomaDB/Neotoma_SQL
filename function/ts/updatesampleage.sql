f0cd841daa051c6cc5e213e59dce4c5d7305a38a
Simon Goring <goring@wisc.edu>
2020-05-31 11:41:01 -0700

    Update the sample age function.

---
+++ function/ts/updatesampleage.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION ts.updatesampleage(_sampleageid integer,
+_age double precision DEFAULT NULL::double precision,
+_ageyounger double precision DEFAULT NULL::double precision,
+_ageolder double precision DEFAULT NULL::double precision)
+ RETURNS void
+ LANGUAGE sql
+AS $function$
+
+  UPDATE ndb.sampleages
+  SET      age = _age,
+      ageyounger = _ageyounger,
+  	    ageolder = _ageolder
+  WHERE sampleageid = _sampleageid;
+
+
+  INSERT INTO ti.stewardupdates(contactid, tablename, pk1, operation)
+  VALUES (_contactid, 'sampleages', _sampleageid, 'update')
+
+
+$function$
