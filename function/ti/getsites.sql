faec20ecaac604307ed8a301b96abe0a8dd5b4b1
Simon Goring <goring@wisc.edu>
2020-07-13 12:40:19 -0700

    Updated `getsites` to deal with geography data.

--- function/ti/getsites.sql
+++ function/ti/getsites.sql
@@ -6,26 +6,30 @@ CREATE OR REPLACE FUNCTION ti.getsites(_datasettypeid integer DEFAULT NULL::inte
             geopolname2 character varying)
  LANGUAGE sql
 AS $function$
+WITH goodgeo AS (
+  SELECT geoout || geoin::integer AS geoarray, geoin FROM ndb.geopaths
+)
 
 SELECT st.siteid,
        st.sitename,
        ST_AsText(st.geog),
        gpa.geopolname1,
        gpb.geopolname2
-from   ndb.sites                    AS st
-  JOIN ndb.dslinks                  AS dsl  ON        dsl.siteid = st.siteid
-  JOIN ndb.sitegeopolitical         AS sgp  ON        sgp.siteid = st.siteid
-  JOIN ndb.datasets                 AS ds   ON      ds.datasetid = dsl.datasetid
+FROM   ndb.sites                    AS st
+  LEFT JOIN ndb.dslinks                  AS dsl  ON        dsl.siteid = st.siteid
+  LEFT JOIN ndb.sitegeopolitical         AS sgp  ON        sgp.siteid = st.siteid
+  LEFT JOIN ndb.datasets                 AS ds   ON      ds.datasetid = dsl.datasetid
   LEFT JOIN ndb.datasetpis          AS dspi ON      ds.datasetid = dspi.datasetid
   LEFT JOIN ndb.datasetpublications AS dsp  ON     dsl.datasetid = dsp.datasetid
   LEFT JOIN ndb.publications        AS pub  ON dsp.publicationid = pub.publicationid
   LEFT JOIN ndb.publicationauthors  AS pua  ON pub.publicationid = pua.publicationid
-  JOIN ti.geopol1                   AS gpa  ON        gpa.siteid = st.siteid
-  JOIN ti.geopol2                   AS gpb  ON        gpb.siteid = st.siteid
+  LEFT JOIN goodgeo AS gg ON gg.geoin = sgp.geopoliticalid
+  LEFT JOIN ti.geopol1                   AS gpa  ON        gpa.siteid = st.siteid
+  LEFT JOIN ti.geopol2                   AS gpb  ON        gpb.siteid = st.siteid
   WHERE
   (_sitename       IS NULL OR LOWER(st.sitename) LIKE LOWER(_sitename)) AND
   (_datasettypeid  IS NULL OR   ds.datasettypeid = _datasettypeid)        AND
-  (_geopoliticalid IS NULL OR sgp.geopoliticalid = _geopoliticalid)     AND
+  (_geopoliticalid IS NULL OR _geopoliticalid = ANY(gg.geoarray))     AND
   (_contactid      IS NULL OR     dspi.contactid = _contactid)              AND
   (_authorid       IS NULL OR      pua.contactid = _authorid)
 GROUP BY st.siteid, st.sitename, st.geog, gpa.geopolname1, gpb.geopolname2
