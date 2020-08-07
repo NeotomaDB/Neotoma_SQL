2d4cb204d6b8ba62536681c1cc21d0af2e171c07
Simon Goring <goring@wisc.edu>
2020-08-07 12:32:14 -0700

    Updated specimen date id in ti.insertspecimendatecal

--- function/ts/insertspecimendatecal.sql
+++ function/ts/insertspecimendatecal.sql
@@ -1,10 +1,10 @@
-CREATE OR REPLACE FUNCTION ts.insertspecimendatecal(_specimendatecalid integer, _calage double precision DEFAULT NULL::double precision, _calageolder double precision DEFAULT NULL::double precision, _calageyounger double precision DEFAULT NULL::double precision, _calibrationcurveid integer DEFAULT NULL::integer, _calibrationprogramid integer DEFAULT NULL::integer, _datecalibrated character varying DEFAULT NULL::character varying)
+CREATE OR REPLACE FUNCTION ts.insertspecimendatecal(_specimendateid integer, _calage double precision DEFAULT NULL::double precision, _calageolder double precision DEFAULT NULL::double precision, _calageyounger double precision DEFAULT NULL::double precision, _calibrationcurveid integer DEFAULT NULL::integer, _calibrationprogramid integer DEFAULT NULL::integer, _datecalibrated character varying DEFAULT NULL::character varying)
  RETURNS integer
  LANGUAGE sql
 AS $function$
-  INSERT INTO ndb.specimendatescal  (specimendatecalid, calage, calageolder,
+  INSERT INTO ndb.specimendatescal  (specimendateid, calage, calageolder,
     calageyounger, calibrationcurveid, calibrationprogramid, datecalibrated)
-  VALUES (_specimendatecalid, _calage, _calageolder, _calageyounger,
+  VALUES (_specimendateid, _calage, _calageolder, _calageyounger,
     _calibrationcurveid, _calibrationprogramid,
     TO_DATE(_datecalibrated, 'YYYY-MM-DD'))
   RETURNING specimendatecalid
