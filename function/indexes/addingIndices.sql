074bd2d6774bb57a9ec4c62e276a9505fd4423ee
Simon Goring <goring@wisc.edu>
2020-08-11 11:16:19 -0700

    Updated indexes.  Implemented on `neotomatilia`. Addresses & resolves issue #7 for now.

--- function/indexes/addingIndices.sql
+++ function/indexes/addingIndices.sql
@@ -1,20 +1,18 @@
-CREATE INDEX   taxonames_idx ON taxa     USING GIN(taxonname gin_trgm_ops);
-CREATE INDEX   sitenames_idx ON sites    USING GIN(sitename gin_trgm_ops);
-CREATE INDEX familynames_idx ON contacts USING GIN(familyname gin_trgm_ops);
+CREATE INDEX IF NOT EXISTS   taxonames_idx ON ndb.taxa     USING GIN(taxonname gin_trgm_ops);
+CREATE INDEX IF NOT EXISTS   sitenames_idx ON ndb.sites    USING GIN(sitename gin_trgm_ops);
+CREATE INDEX IF NOT EXISTS familynames_idx ON ndb.contacts USING GIN(familyname gin_trgm_ops);
 
-CREATE INDEX sitegeog_gix ON sites USING GIST (geog);
+CREATE INDEX IF NOT EXISTS sitegeog_gix ON ndb.sites USING GIST (geog);
 
-CREATE INDEX youngage_idx ON chronologies USING btree(ageboundyounger);
+CREATE INDEX IF NOT EXISTS chryoungage_idx ON ndb.chronologies USING btree(ageboundyounger);
+CREATE INDEX IF NOT EXISTS chroldage_idx ON ndb.chronologies USING btree(ageboundolder);
 
-CREATE INDEX oldage_idx ON chronologies USING btree(ageboundolder);
-CREATE INDEX youngage_idx ON chronologies USING btree(ageboundyounger);
+CREATE INDEX IF NOT EXISTS ccrchronage_idx ON ndb.chroncontrols USING btree(age);
+CREATE INDEX IF NOT EXISTS ccroldage_idx ON ndb.chroncontrols USING btree(agelimitolder);
+CREATE INDEX IF NOT EXISTS ccryoungage_idx ON ndb.chroncontrols USING btree(agelimityounger);
 
-CREATE INDEX oldage_idx ON chroncontrols USING btree(agelimitolder);
-CREATE INDEX youngage_idx ON chroncontrols USING btree(agelimityounger);
+CREATE INDEX IF NOT EXISTS smpage_idx ON ndb.sampleages USING btree(age);
+CREATE INDEX IF NOT EXISTS smpageold_idx ON ndb.sampleages USING btree(ageolder);
+CREATE INDEX IF NOT EXISTS smpageyoung_idx ON ndb.sampleages USING btree(ageyounger);
 
-CREATE INDEX smpage_idx ON sampleages USING btree(age);
-CREATE INDEX smpageold_idx ON sampleages USING btree(ageolder);
-CREATE INDEX smpageyoung_idx ON sampleages USING btree(ageyounger);
-
-CREATE INDEX chronage_idx ON chroncontrols USING btree(age);
-CREATE INDEX geoage_idx ON geochronology USING btree(age);
+CREATE INDEX IF NOT EXISTS geoage_idx ON ndb.geochronology USING btree(age);
