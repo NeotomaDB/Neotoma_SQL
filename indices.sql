c53bbf35743ca7fbf84afd3217d5fac370a7c8cc
Simon Goring <goring@wisc.edu>
2020-08-11 10:57:23 -0700

    Moved indexes out of the root folder and into a folder in `function`. This generates a set of indexes, based in part on the table in issue #7.

--- indices.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX taxonname_btree ON ndb.taxa USING btree (taxonname)
-CREATE INDEX sitegeog_gix ON sites USING GIST (geog);
-CREATE INDEX youngage_idx ON chronologies USING btree(ageboundyounger)
-CREATE INDEX oldage_idx ON chronologies USING btree(ageboundolder)
