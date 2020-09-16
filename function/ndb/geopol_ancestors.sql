36a7fab951eb2b710487bd197c378e0aca099cd6
Simon Goring <goring@wisc.edu>
2020-09-16 15:32:16 -0700

    For geopaths added grant permissions and madesure duplicate adds don't happen.

--- function/ndb/geopol_ancestors.sql
+++ function/ndb/geopol_ancestors.sql
@@ -1,19 +1,23 @@
-CREATE TABLE ndb.geopaths (
+CREATE TABLE IF NOT EXISTS ndb.geopaths (
   geoout INTEGER[] NOT NULL,
   geoin BIGINT NOT NULL,
   PRIMARY KEY (geoout, geoin),
   FOREIGN KEY (geoin) REFERENCES ndb.geopoliticalunits(geopoliticalid)
 );
 
+GRANT SELECT ON ndb.geopaths TO neotomawsreader;
+GRANT SELECT ON ndb.geopaths TO neotomawswriter;
+
 INSERT INTO ndb.geopaths(geoout, geoin)
   WITH RECURSIVE geopol AS (
-  	  SELECT gp.geopoliticalid, ARRAY[]::integer[] AS ancestors
-        FROM ndb.geopoliticalunits AS gp
-  	  WHERE gp.highergeopoliticalid = 0
+    SELECT gp.geopoliticalid, ARRAY[]::integer[] AS ancestors
+      FROM ndb.geopoliticalunits AS gp
+        LEFT OUTER JOIN ndb.geopaths AS geop ON geop.geoin = gp.geopoliticalid
+      WHERE gp.highergeopoliticalid = 0 AND geop.geoin IS NULL
     UNION ALL
   	  SELECT gp.geopoliticalid, geopol.ancestors || gp.highergeopoliticalid
   	  FROM ndb.geopoliticalunits AS gp, geopol
   	  WHERE gp.highergeopoliticalid = geopol.geopoliticalid)
   SELECT geo.ancestors AS geoout,
   	   geo.geopoliticalid AS geoin
-  FROM geopol AS geo
+  FROM geopol AS geo;
