1f239c3e2fc21a69527276e68d6ef7679805f6bc
Simon Goring <goring@wisc.edu>
2020-09-28 13:13:29 -0700

    Updating the sequencing data and removing steward table updates.

--- function/ts/insertsample.sql
+++ function/ts/insertsample.sql
@@ -1,4 +1,12 @@
-CREATE OR REPLACE FUNCTION ts.insertsample(_analysisunitid integer, _datasetid integer, _samplename character varying DEFAULT NULL::character varying, _sampledate character varying DEFAULT NULL::character varying, _analysisdate character varying DEFAULT NULL::character varying, _taxonid integer DEFAULT NULL::integer, _labnumber character varying DEFAULT NULL::character varying, _prepmethod character varying DEFAULT NULL::character varying, _notes character varying DEFAULT NULL::character varying)
+CREATE OR REPLACE FUNCTION ts.insertsample(_analysisunitid integer,
+  _datasetid integer,
+  _samplename character varying DEFAULT NULL::character varying,
+  _sampledate character varying DEFAULT NULL::character varying,
+  _analysisdate character varying DEFAULT NULL::character varying,
+  _taxonid integer DEFAULT NULL::integer,
+  _labnumber character varying DEFAULT NULL::character varying,
+  _prepmethod character varying DEFAULT NULL::character varying,
+  _notes character varying DEFAULT NULL::character varying)
  RETURNS integer
  LANGUAGE sql
 AS $function$
--- helpers/reset_sequences.sql
+++ helpers/reset_sequences.sql
@@ -1,3 +1,4 @@
+/* Code from here: */
 SELECT 'select '
         || trim(trailing ')'
            from replace(pg_get_expr(d.adbin, d.adrelid),
@@ -5,10 +6,10 @@ SELECT 'select '
         || ', (select max( ' || a.attname || ') from only '
         || nspname || '.' || relname || '));'
   FROM pg_class c
-       JOIN pg_namespace n on n.oid = c.relnamespace
-       JOIN pg_attribute a on a.attrelid = c.oid
-       JOIN pg_attrdef d on d.adrelid = a.attrelid
-                          and d.adnum = a.attnum
-                          and a.atthasdef
- WHERE relkind = 'r' and a.attnum > 0
-       and pg_get_expr(d.adbin, d.adrelid) ~ '^nextval';
+       JOIN pg_namespace n ON n.oid = c.relnamespace
+       JOIN pg_attribute a ON a.attrelid = c.oid
+       JOIN pg_attrdef d ON d.adrelid = a.attrelid
+                          AND d.adnum = a.attnum
+                          AND a.atthasdef
+WHERE relkind = 'r' and a.attnum > 0
+  AND pg_get_expr(d.adbin, d.adrelid) ~ '^nextval';
