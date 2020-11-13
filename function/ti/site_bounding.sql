d02c51dc982007512a8212ceaf8a5267e7202696
SimonGoring <goring@wisc.edu>
2020-11-12 19:15:38 -0800

    Moved the trigger function `site_bounding` to a file in the `ti` folder Edited the Trigger to reflect this.

---
+++ function/ti/site_bounding.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION ti.site_bounding() RETURNS TRIGGER
+AS $$
+BEGIN
+   IF NEW.latitudenorth IS NULL AND NEW.geog IS NOT NULL THEN
+   UPDATE ndb.sites
+   SET (longitudeeast, latitudenorth, longitudewest, latitudesouth) =
+     (ST_XMax(geog::geometry),
+      ST_YMax(geog::geometry),
+      ST_XMin(geog::geometry),
+      ST_YMin(geog::geometry));
+   END IF;
+   RETURN NEW;
+END;
+
+$$ LANGUAGE plpgsql;
--- function/triggers/sitegeoms.sql
+++ function/triggers/sitegeoms.sql
@@ -1,20 +1,5 @@
-CREATE OR REPLACE FUNCTION ti.site_bounding() RETURNS TRIGGER
-AS $$
-BEGIN
-   IF NEW.latitudenorth IS NULL AND NEW.geom IS NOT NULL THEN
-   UPDATE ndb.sites
-   SET (longitudeeast, latitudenorth, longitudewest, latitudesouth) =
-     (ST_XMax(geog::geometry),
-      ST_YMax(geog::geometry),
-      ST_XMin(geog::geometry),
-      ST_YMin(geog::geometry));
-   END IF;
-   RETURN NEW;
-END;
-
-$$ LANGUAGE plpgsql;
-
 CREATE TRIGGER updatelocations
-AFTER INSERT ON ndb.sites
+AFTER INSERT OR UPDATE OR DELETE
+ON ndb.sites
 FOR EACH ROW
 EXECUTE PROCEDURE ti.site_bounding()
