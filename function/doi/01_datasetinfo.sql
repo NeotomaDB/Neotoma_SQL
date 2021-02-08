7a43fdf3581d2416e6178126633dc8ffe26efe7d
Simon Goring <simon.j.goring@gmail.com>
2021-02-08 12:27:13 -0800

    Updating the DOI/API requests for downloads.

--- function/doi/01_datasetinfo.sql
+++ function/doi/01_datasetinfo.sql
@@ -6,6 +6,7 @@ AS $function$
 SELECT dts.datasetid,
        json_build_object('site', json_build_object('siteid', sts.siteid,
                                                  'sitename', sts.sitename,
+                                  'depositionalenvironment', dvt.depenvt,
                                           'sitedescription', sts.sitedescription,
                                                 'sitenotes', sts.notes,
                                                 'geography', ST_AsGeoJSON(sts.geog,5,2),
@@ -22,16 +23,17 @@ SELECT dts.datasetid,
 																                    'datasetpi', dsau.authors,
 																                      'agerange', agerange.ages))
 FROM
-ndb.datasets AS dts LEFT OUTER JOIN
-ndb.collectionunits AS clu ON clu.collectionunitid = dts.collectionunitid LEFT OUTER JOIN
-ndb.sites AS sts ON sts.siteid = clu.siteid  LEFT OUTER JOIN
-ndb.datasettypes AS dst ON dst.datasettypeid = dts.datasettypeid LEFT OUTER JOIN
-ndb.collectiontypes as cts ON clu.colltypeid = cts.colltypeid LEFT OUTER JOIN
+         ndb.datasets AS dts LEFT OUTER JOIN
+  ndb.collectionunits AS clu ON clu.collectionunitid = dts.collectionunitid LEFT OUTER JOIN
+     ndb.depenvttypes AS dvt ON dvt.depenvtid = clu.depenvtid LEFT OUTER JOIN
+            ndb.sites AS sts ON sts.siteid = clu.siteid  LEFT OUTER JOIN
+     ndb.datasettypes AS dst ON dst.datasettypeid = dts.datasettypeid LEFT OUTER JOIN
+  ndb.collectiontypes AS cts ON clu.colltypeid = cts.colltypeid LEFT OUTER JOIN
 ndb.datasetdatabases AS dsdb ON dsdb.datasetid = dts.datasetid LEFT OUTER JOIN
-(SELECT * FROM doi.agerange(dsid)) AS agerange ON agerange.datasetid = dts.datasetid LEFT OUTER JOIN
-ndb.constituentdatabases AS cstdb ON dsdb.databaseid = cstdb.databaseid LEFT OUTER JOIN
-(SELECT * FROM doi.doireturn(dsid)) AS doi ON doi.datasetid = dts.datasetid LEFT OUTER JOIN
-(SELECT * FROM doi.datasetauthors(dsid)) AS dsau ON dsau.datasetid = dts.datasetid
+   (SELECT * FROM doi.agerange(dsid)) AS agerange ON agerange.datasetid = dts.datasetid LEFT OUTER JOIN
+               ndb.constituentdatabases  AS cstdb ON dsdb.databaseid = cstdb.databaseid LEFT OUTER JOIN
+    (SELECT * FROM doi.doireturn(dsid))  AS doi   ON doi.datasetid = dts.datasetid LEFT OUTER JOIN
+(SELECT * FROM doi.datasetauthors(dsid)) AS dsau  ON dsau.datasetid = dts.datasetid
 WHERE dts.datasetid = ANY(dsid)
 
 $function$;
@@ -45,6 +47,7 @@ AS $function$
 SELECT dts.datasetid,
        json_build_object('site', json_build_object('siteid', sts.siteid,
                                                  'sitename', sts.sitename,
+                                  'depositionalenvironment', dvt.depenvt,
                                           'sitedescription', sts.sitedescription,
                                                 'sitenotes', sts.notes,
                                                 'geography', ST_AsGeoJSON(sts.geog,5,2),
@@ -63,6 +66,7 @@ SELECT dts.datasetid,
 FROM
 ndb.datasets AS dts LEFT OUTER JOIN
 ndb.collectionunits AS clu ON clu.collectionunitid = dts.collectionunitid LEFT OUTER JOIN
+     ndb.depenvttypes AS dvt ON dvt.depenvtid = clu.depenvtid LEFT OUTER JOIN
 ndb.sites AS sts ON sts.siteid = clu.siteid  LEFT OUTER JOIN
 ndb.datasettypes AS dst ON dst.datasettypeid = dts.datasettypeid LEFT OUTER JOIN
 ndb.collectiontypes as cts ON clu.colltypeid = cts.colltypeid LEFT OUTER JOIN
