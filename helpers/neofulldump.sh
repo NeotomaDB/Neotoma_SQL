cd65b1c01869f093103ba1605bd77c0af254fc45
Simon Goring <simon.j.goring@gmail.com>
2021-02-25 16:05:22 -0800

    Updating the helper functions Updated added a view for fulltext search.

--- helpers/neofulldump.sh
+++ helpers/neofulldump.sh
@@ -7,7 +7,8 @@ fi
 now=`date +"%Y-%m-%d"`
 mkdir -p dumps
 mkdir -p archives
-pg_dump -Fc -O -h db5.cei.psu.edu -U $1 -N gen -N tmp -W -v -d neotoma > ./dumps/neotoma_dump_full_${now}.sql
+pg_dump -Fc -O -v -h db5.cei.psu.edu -U $1 -s neotoma > ./dumps/neotoma_ndb_schema_${now}.sql
+pg_dump -Fc -O -v -h db5.cei.psu.edu -U $1 -N gen -N tmp -W -v -d neotoma > ./dumps/neotoma_dump_full_${now}.sql
 tar -cvf ./archives/neotoma_dump_full_${now}.tar -C ./dumps neotoma_dump_full_${now}.sql
 rm ./dumps/neotoma_dump_full_${now}.sql
 pg_dump -Fc -O -h db5.cei.psu.edu -U $1 -N gen -n ndb -W -v -d neotoma > ./dumps/neotoma_dump_ndb_${now}.sql
