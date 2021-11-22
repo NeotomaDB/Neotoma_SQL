72602abee549e0a638eaa965badbe0fa41c76000
Simon Goring <simon.j.goring@gmail.com>
2021-11-22 15:23:07 -0800

    Adding the chron control thickness function.

--- function/ts/updatechroncontroldepththickness.sql
+++ function/ts/updatechroncontroldepththickness.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE FUNCTION ts.updatechroncontroldepththickness({_chroncontrolid integer,_depth integer,_thickness integer,_analunitid integer})
+CREATE OR REPLACE FUNCTION ts.updatechroncontroldepththickness(_chroncontrolid integer,_depth integer,_thickness integer,_analunitid integer)
  RETURNS void
  LANGUAGE sql
 AS $function$
 	UPDATE ndb.chroncontrols AS cc
 	SET   depth = _depth, thickness = _thickness
 	WHERE cc.chroncontrolid = _chroncontrolid AND analysisunitid = _analunitid;
-$function$
+$function$;
