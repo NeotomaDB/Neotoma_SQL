5973e295587daad20d8ffc7c832ce49242183637
SimonGoring <goring@wisc.edu>
2023-03-16 12:48:42 -0700

    Removed steward udate table.

--- function/ts/insertdatasettaxonnotes.sql
+++ function/ts/insertdatasettaxonnotes.sql
@@ -4,16 +4,4 @@ CREATE OR REPLACE FUNCTION ts.insertdatasettaxonnotes(_datasetid integer, _taxon
 AS $function$
   INSERT INTO ndb.datasettaxonnotes (datasetid, taxonid, contactid, date, notes)
   VALUES      (_datasetid, _taxonid, _contactid, TO_DATE(_date, 'YYYY-MM-DD'), _notes);
-
-  WITH updval AS (
-    SELECT _contactid AS contactid,
-           'datasettaxonnotes' AS tablename,
-           _datasetid AS pk1,
-           _taxonid AS pk2,
-           'insert' AS operation
-    WHERE _update
-  )
-  INSERT INTO ti.stewardupdates(contactid, tablename, pk1, pk2, operation)
-  SELECT * FROM updval;
-
 $function$
