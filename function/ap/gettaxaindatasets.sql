60b206aaec6f7b969156f73d3acf38d8116ea541
SimonGoring <goring@wisc.edu>
2023-03-20 12:12:51 -0700

    Updated ap.gettaxaindatasets to include jsonb
    The doindbdata function also now has an added integer[] method.

--- function/ap/gettaxaindatasets.sql
+++ function/ap/gettaxaindatasets.sql
@@ -8,11 +8,11 @@ AS $function$
     SELECT t.taxonid,
            t.taxonname,
            t.taxagroupid,
-           dtgt.datasettypeid
+           jsonb_agg(dtgt.datasettypeid) as datasettypeid
     FROM ndb.taxa AS t
     LEFT JOIN ndb.datasettaxagrouptypes AS dtgt ON t.taxagroupid = dtgt.taxagroupid
     INNER JOIN ndb.variables AS v ON t.taxonid = v.taxonid
     WHERE t.valid = True
-    GROUP BY t.taxonid, t.taxonname, dtgt.datasettypeid, t.taxagroupid
-    ORDER BY t.taxonname, dtgt.datasettypeid
-$function$
+    GROUP BY t.taxonid, t.taxonname, t.taxagroupid
+    ORDER BY t.taxonname
+$function$;
