9201449cbd7ed3efa278a0433ab09f4b20733c1b
SimonGoring <goring@wisc.edu>
2023-04-13 14:55:45 -0700

    Updating trigger and deletesite function
    The ts.delete site wasn't working. The main issue seemed to be with the trigger, which was trying to update tuples on delete. There is no NEW on delete, so the trigger function was failing.

--- function/ts/deletesite.sql
+++ function/ts/deletesite.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION ts.deletesite(_siteid integer)
+CREATE OR REPLACE FUNCTION ts.deletesite(:_siteid integer)
  RETURNS void
  LANGUAGE sql
 AS $function$
@@ -7,11 +7,11 @@ AS $function$
     SELECT  gcc.chroncontrolid,
                 gcc.geochronid
     FROM    ndb.sites            AS sts
-    JOIN    ndb.collectionunits  AS cu  ON sts.siteid = cu.siteid
-    JOIN    ndb.chronologies     AS chr ON cu.collectionunitid = chr.collectionunitid
-    JOIN    ndb.chroncontrols    AS cc  ON chr.chronologyid = cc.chronologyid
-    JOIN    ndb.geochroncontrols AS gcc ON cc.chroncontrolid = gcc.chroncontrolid
-    WHERE   sts.siteid = _siteid
+    LEFT JOIN    ndb.collectionunits  AS cu  ON sts.siteid = cu.siteid
+    LEFT JOIN    ndb.chronologies     AS chr ON cu.collectionunitid = chr.collectionunitid
+    LEFT JOIN    ndb.chroncontrols    AS cc  ON chr.chronologyid = cc.chronologyid
+    LEFT JOIN    ndb.geochroncontrols AS gcc ON cc.chroncontrolid = gcc.chroncontrolid
+    WHERE   sts.siteid = :_siteid
   )
 
 	DELETE FROM ndb.geochroncontrols AS gcc
@@ -20,9 +20,9 @@ AS $function$
   WITH chids AS (
     SELECT  chr.chronologyid
     FROM    ndb.sites AS sts
-    JOIN    ndb.collectionunits AS cu  ON sts.siteid = cu.siteid
-    JOIN    ndb.chronologies    AS chr ON cu.collectionunitid = chr.collectionunitid
-    WHERE   sts.siteid = _siteid
+    LEFT JOIN    ndb.collectionunits AS cu  ON sts.siteid = cu.siteid
+    LEFT JOIN    ndb.chronologies    AS chr ON cu.collectionunitid = chr.collectionunitid
+    WHERE   sts.siteid = :_siteid
   )
 
 	DELETE FROM ndb.chronologies AS chr WHERE chr.chronologyid IN (SELECT chronologyid FROM chids);
@@ -30,10 +30,10 @@ AS $function$
   WITH samps AS (
     SELECT     smp.sampleid
     FROM       ndb.sites AS sts
-    JOIN ndb.collectionunits AS cu  ON sts.siteid = cu.siteid
-    JOIN ndb.analysisunits AS au ON cu.collectionunitid = au.collectionunitid
-    JOIN ndb.samples AS smp ON au.analysisunitid = smp.analysisunitid
-    WHERE sts.siteid = _siteid
+    LEFT JOIN ndb.collectionunits AS cu  ON sts.siteid = cu.siteid
+    LEFT JOIN ndb.analysisunits AS au ON cu.collectionunitid = au.collectionunitid
+    LEFT JOIN ndb.samples AS smp ON au.analysisunitid = smp.analysisunitid
+    WHERE sts.siteid = :_siteid
   )
 
   DELETE FROM ndb.samples AS smp WHERE smp.sampleid IN (SELECT sampleid FROM samps);
@@ -41,14 +41,14 @@ AS $function$
   WITH aus AS (
     SELECT     au.analysisunitid
     FROM       ndb.sites AS sts
-    JOIN ndb.collectionunits AS cu ON sts.siteid = cu.siteid
-    JOIN ndb.analysisunits AS au ON cu.collectionunitid = au.collectionunitid
-    WHERE sts.siteid = _siteid
+    LEFT JOIN ndb.collectionunits AS cu ON sts.siteid = cu.siteid
+    LEFT JOIN ndb.analysisunits AS au ON cu.collectionunitid = au.collectionunitid
+    WHERE sts.siteid = :_siteid
   )
 
   DELETE FROM ndb.analysisunits AS au WHERE au.analysisunitid  IN (SELECT analysisunitid FROM aus);
 
   DELETE FROM ndb.sites AS sts
-  WHERE sts.siteid = _siteid
+  WHERE sts.siteid = :_siteid;
 
 $function$
